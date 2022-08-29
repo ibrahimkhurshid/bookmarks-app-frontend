ab6c6688430532747a24cce1804f571c2530df2e
Ibrahim Khurshid <ibrahimkhurshid@hotmail.com>
2022-08-30 00:34:31 +0500

    Adding heroku deploy as new host

--- src/BookmarkList.js
+++ src/BookmarkList.js
@@ -2,20 +2,47 @@ import React from "react";
 import Bookmark from "./Bookmark";
 import { Link } from "react-router-dom";
 import "./BookmarkList.css";
+import QRCodeStyling from "qr-code-styling";
+import { BiBarcodeReader } from "react-icons/bi";
 
 const BookmarkList = (props) => {
   const { list, deleteCallback } = props;
+
+  const qrCode = new QRCodeStyling({
+    width: 300,
+    height: 300,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+    dotsOptions: {
+      color: "#444",
+      type: "rounded",
+    },
+    imageOptions: {
+      crossOrigin: "anonymous",
+      margin: 20,
+    },
+  });
 
   const genList = (list) => {
     return list.map((bm, id) => {
       return (
         <tr>
+          <Bookmark props={{ id: bm.id, url: bm.url, title: bm.title }} />
           <td>
-            <Bookmark props={{ id: bm.id, url: bm.url, title: bm.title }} />
+            <Link to={String(bm.id)}>Details</Link>
           </td>
           <td>
-            <Link to={String(bm.id)}>Show</Link>
             <button onClick={() => deleteCallback(bm.id)}>delete</button>
+          </td>
+          <td>
+            <BiBarcodeReader
+              onClick={() => {
+                qrCode.update({
+                  data: bm.url,
+                });
+                qrCode.download({ extension: "png" });
+              }}
+            />
           </td>
         </tr>
       );
@@ -25,13 +52,12 @@ const BookmarkList = (props) => {
   return (
     <table>
       <thead>
-        <tr>
-          <tr>
-            <th>id</th>
-            <th>url</th>
-            <th>title</th>
-          </tr>
-        </tr>
+        <th>id</th>
+        <th>url</th>
+        <th>title</th>
+        <th>View</th>
+        <th>Delete</th>
+        <th>QR Code</th>
       </thead>
       <tbody>{genList(list)}</tbody>
     </table>
--- src/env.js
+++ src/env.js
@@ -1,1 +1,2 @@
-export const HOST = "http://localhost:3001/";
+// export const HOST = "http://192.168.99.43:3001/";
+export const HOST = "https://bookmarks-nestjs.herokuapp.com/";
